2fa9fbb80ebe8e51175a2e4dd629851ab897b6c1
gautam2712 <103632594+gautam2712@users.noreply.github.com>
2022-12-13 13:34:43 +0530

    .

---
+++ C-language/01-simpal/02-equation-type/ascii.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    char ch;
+    printf("enter charter = ");
+    scanf("%c",&ch);
+    printf("ascii value of chareter = %d\n",ch);
+    printf("charcter = ");
+    putchar(ch);
+}
